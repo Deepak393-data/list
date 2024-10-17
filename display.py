532281623eaf890f3289aa2ff0829b0633566d7a
Deepak393-data <22hr1a3248@mtieat.org>
2024-10-17 01:20:55 -0700

    Create display.py

---
+++ display.py
@@ -0,0 +1,30 @@
+'''
+Write a program to create a list and display it. 
+Input format:
+Input consist of n+1 integers
+First integer corresponds to the size of the list
+Next n inputs corresponds to the elements in the list 
+Output format: 
+Output is an integer list
+Sample Input
+4 
+1
+2
+3
+4
+Output
+1 2 3 4
+'''
+# Input the size of the list
+n = int(input())
+
+# Initialize an empty list
+my_list = []
+
+# Input the elements of the list
+for _ in range(n):
+    element = int(input())
+    my_list.append(element)
+
+# Output the list elements in the required format
+print(" ".join(map(str, my_list)))
